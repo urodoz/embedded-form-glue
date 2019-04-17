917cabe241559406c49fae0053929d1fc3271154
Albert Lacarta <albert.lacarta@lyra-network.com>
2019-04-17 12:27:08 +0200

    Tests

--- tests/test_root.py
+++ tests/test_root.py
@@ -1,9 +1,13 @@
-import time
+import os
 import unittest
 
+from selenium import webdriver
 from selenium.webdriver import Firefox
-from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.firefox.options import Options
+
+REMOTE_SELENIUM = os.getenv("REMOTE_SELENIUM", None)
 
 def factory_firefox():
     driver = Firefox()
@@ -12,10 +16,16 @@ def factory_firefox():
 class TestRoot(unittest.TestCase):
 
     def setUp(self):
-        self.selenium = factory_firefox()
+        if not REMOTE_SELENIUM:
+            self.selenium = factory_firefox()
+        else:
+            driver = webdriver.Remote(
+                command_executor='http://{0}:4444/wd/hub'.format(REMOTE_SELENIUM),
+                desired_capabilities=DesiredCapabilities.FIREFOX)
+            self.selenium = driver
 
     def test_failed(self):
-        self.selenium.get("http://127.0.0.1:8000/tests/views/index.html")
+        self.selenium.get("{0}/tests/views/index.html".format(os.getenv("WEB_HOST")))
         window_keys = self.selenium.execute_script(u"""
         return Object.keys(window);
         """)
